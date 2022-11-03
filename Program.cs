1eb31fab46154ba35533d7618a507001d0e75999
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-11-02 19:32:34 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,7 +12,57 @@ namespace Lecture_10_TryParse_Methods
         {
             // TryParse
 
+            //try
+            //{
+            //    int value = int.Parse("ten");
+            //    Console.WriteLine("Print this line");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Your exception was {ex.Message}");
+            //}
+
+            Console.Write("Enter a number: ");
+
+            int usersNumber = -1;
+            bool correctInput = int.TryParse(Console.ReadLine(), out usersNumber);
+
+            if(correctInput)
+            {
+                Console.WriteLine(usersNumber * usersNumber);
+            }
+            else
+            {
+                Console.WriteLine("That was an invalid input");
+            }
+
+
+            //    int theNumber = 0;
+            //bool isANumber = int.TryParse("ten", out theNumber);
+            //if(isANumber)
+            //{
+            //    Console.WriteLine("That was a valid number");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("That was an invalid number");
+            //}
+            //Console.WriteLine($"The value of result {theNumber}");
+
+            //try
+            //{
+            //    int number = int.Parse("dog");
+
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("Enter a valid number");
+            //}
+
             // Methods
+
+            Console.ReadKey();
         }
     }
 }
